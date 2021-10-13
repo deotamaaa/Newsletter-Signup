d4c808dbd3d01ece186930d1fa784f0ce72ef670
Deotama <deotama11@gmail.com>
2021-10-13 18:51:04 +0700

    Second Commit

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 const express = require("express");
+const request = require("request");
+const path = require("path");
 
 const app = express();
+
+app.use("*/css", express.static("public/css"));
+app.use("*/images", express.static("public/images"));
+
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/signup.html");
+});
 
 app.listen(3000, function () {
   console.log("Server is running");
